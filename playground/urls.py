f6c7e4c9110b2690c182cdd314a652da42ebbbe8
Michal Marciniak <gp3510@gmail.com>
2023-01-06 09:56:53 +0100

    using basic templates

--- playground/urls.py
+++ playground/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("hello/", views.say_hello)]
+urlpatterns = [
+    path("hello/", views.say_hello),
+    path("hello-template/", views.say_hello_template),
+    path("hello-template-context/", views.say_hello_template_with_context),
+]
--- playground/views.py
+++ playground/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 
 def say_hello(request):
     return HttpResponse("Hello World")
+
+
+def say_hello_template(request):
+    return render(request, "hello.html")
+
+
+def say_hello_template_with_context(request):
+    return render(
+        request, "hello_context.html", {"name": "John", "lastName": "Doe"}
+    )
