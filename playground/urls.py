8f0b3e694fc3bd633313325f8b607d7a1955c1a4
Michal Marciniak <gp3510@gmail.com>
2023-01-07 14:04:28 +0100

    simple test for vsc debugging

--- playground/urls.py
+++ playground/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("hello/", views.say_hello),
     path("hello-template/", views.say_hello_template),
     path("hello-template-context/", views.say_hello_template_with_context),
+    path("debug-test/", views.testing_debug),
 ]
--- playground/views.py
+++ playground/views.py
@@ -14,3 +14,14 @@ def say_hello_template_with_context(request):
     return render(
         request, "hello_context.html", {"name": "John", "lastName": "Doe"}
     )
+
+
+def testing_debug(request):
+    x = calculate()
+    return HttpResponse(f"Result for test calculation: {x}")
+
+
+def calculate():
+    x = 1
+    y = 2
+    return x + y
